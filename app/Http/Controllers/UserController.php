0bedd42efee8d289e699604e02a27e55c4aa474e
Jahongir <alimovj727@gmail.com>
2025-02-21 19:12:18 +0500

    ⚡ Improve error handling in API responses –

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,47 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+  
+    public function show(User $user)
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json([
+            'status' => 200,
+            'data' => [
+                'user' => $user,
+                'posts' => $user->posts
+            ]
+        ]);
     }
 }
