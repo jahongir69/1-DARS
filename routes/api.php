2414f26408a99a46583b95c59042e74dffb25c99
Jahongir <alimovj727@gmail.com>
2025-02-21 19:12:53 +0500

    🚀 Optimize query performance for user posts –

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
